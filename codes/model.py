dbc61b6d3da5f34992634ee49b4d3ece17a269d9
alifthi <alifthi@debian.alifthi>
2023-08-06 12:54:35 -0400

    image_imagecaptioning: model script defined to handels oprations that relates to models

---
+++ codes/model.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import transformers
+class Model:
+    def __init__(self) -> None:
+        self.imageEncoderModel=None
+        self.textEncoderModel=None
+        self.FFN=None
+    def compileModel(self):
+        pass
+    def trainModel(self):
+        pass
+    def buildImageEncoder(self):
+        pass
+    def buildTextEncoder(self):
+        pass
+    def buildFeedForwardnet(self):
+        pass
+    
