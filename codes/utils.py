74f5488b8a099c3709dffcfa5027520e3d81e93f
alifthi <alifthi@debian.alifthi>
2023-08-06 13:01:56 -0400

    image_imagecaptioning: utils class defined to handels opration the oprations that related to data

---
+++ codes/utils.py
@@ -0,0 +1,13 @@
+class Utils:
+    def __init__(self) -> None:
+        pass
+    def loadData(self):
+        pass
+    def downloadData(self):
+        pass
+    def textPreprocessing(self):
+        pass
+    def imagePreprocessing(self):
+        pass
+    def textTokenization(self):
+        pass
